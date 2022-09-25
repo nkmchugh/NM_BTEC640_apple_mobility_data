ffb8e1d432df3a7eb33d7ee9ac2dc3d6afbc6dec
Nicole McHugh <nkmchugh@dons.usfca.edu>
2022-09-24 17:27:11 -0700

    Add for loop function to output five chosen states

--- code/01_subset_data.R
+++ code/01_subset_data.R
@@ -1,16 +1,20 @@
 # Script to read in Apple mobility data from a csv file
-# and subset the data to just include rows from the chosen state
-# and then write out a csv file that only has that subset, using functions in 
+# and subset the data to just include rows from the five chosen states 
+# and then write out a csv files that only has that subset, using functions in 
 # the code/function directory
 
 # Nicole McHugh
 # nkmchugh@dons.usfca.edu
-# September 18, 2022
+# September 24, 2022
 
 # load the function
 source("code/functions/01_subset_data_to_a_state.R")
 
-# run the subset function
-subset_data_to_a_state(
-  input_file_name = "data/raw_data/applemobilitytrends-2022-04-12.csv",
-  state_name = "California")
+# run the subset function with the for loop function
+for (sub_region in c("California", "Hawaii", "Alaska", "Arizona", 
+                     "Washington")) {
+  subset_data_to_a_state(input_file_name = paste0("data/raw_data/",
+                                                 "applemobilitytrends-2022",
+                                                 "-04-12.csv"),
+                         state_name = sub_region)
+}
