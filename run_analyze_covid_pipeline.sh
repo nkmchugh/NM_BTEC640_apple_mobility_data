976d496012aaa93db2e457c1e95a32d485277702
Nicole McHugh <nkmchugh@dons.usfca.edu>
2022-10-22 21:32:40 -0700

    Add different state to bash script

--- run_analyze_covid_pipeline.sh
+++ run_analyze_covid_pipeline.sh
@@ -3,4 +3,6 @@
 # A bash script to drive the rendering of an Rmarkdown file using some 
 # parameters passed in from the shell
 
-Rscript -e "rmarkdown::render('analysis.Rmd')"
+Rscript -e "rmarkdown::render('analysis.Rmd',\
+            params = list(state = 'Arizona',\
+            data = 'data/raw_data/applemobilitytrends-2022-04-12.csv'))"
